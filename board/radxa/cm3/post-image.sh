9b56c0cb0025ecf97788406257ffd5f57fad472d
Leander Wollersberger <leander.wollersberger@gmx.at>
2025-08-17 21:23:39 +0200

    Initial addition of radxa cm3 files

---
+++ board/radxa/cm3/post-image.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Copy DTB(s) manually to the boot partition or boot output directory
+# Adjust the path to where Buildroot places your DTB(s)
+
+# Example: if you're using the device tree from the kernel
+DTB_SRC="${BUILD_DIR}/linux-6.16/arch/arm64/boot/dts/rockchip"
+DTB_DST="${TARGET_DIR}/boot"
+DTB_FILE="rk3566-radxa-cm3-io.dtb"
+
+mkdir -p "$DTB_DST"
+
+# Copy specific DTB(s) or all
+cp "$DTB_SRC"/"$DTB_FILE" "$DTB_DST/"
